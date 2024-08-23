94f492c15d51b96ad3fc8f4f3205ab6bc109c91a
Jose Aureo <jose.sleite@pagar.me>
2024-08-23 06:51:59 -0300

    feature: add checkout test

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -5,31 +5,36 @@ class CheckoutPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def go_direct_to_checkout(self):
-        self.driver.get("https://www.saucedemo.com/checkout-step-one.html")
+    def click_checkout_button(self):
+        self.driver.find_element(By.ID, "checkout").click()
 
-    def fill_checkout_form(self, first_name, last_name, address):
-        self.driver.find_element(By.ID, "first-name").send_keys(first_name)
-        self.driver.find_element(By.ID, "last-name").send_keys(last_name)
-        self.driver.find_element(By.ID, "postal-code").send_keys(address)
-
-    def next_checkout_step(self):
-        self.driver.find_element(By.ID, "continue").click()
-
-    def finish_checkout(self):
-        self.driver.find_element(By.ID, "finish").click()
-
-    def is_on_checkout_step_two(self):
-        return (
-            self.driver.current_url
-            == "https://www.saucedemo.com/checkout-step-two.html"
+    def enter_first_name(self, first_name):
+        self.driver.find_element(By.CSS_SELECTOR, '[data-test="firstName"]').send_keys(
+            first_name
         )
 
-    def is_on_checkout_complete(self):
-        return (
-            self.driver.current_url
-            == "https://www.saucedemo.com/checkout-complete.html"
-            and self.driver.find_element(
-                By.ID, "checkout_complete_container"
-            ).is_displayed()
+    def enter_last_name(self, last_name):
+        self.driver.find_element(By.CSS_SELECTOR, '[data-test="lastName"]').send_keys(
+            last_name
         )
+
+    def enter_postal_code(self, postal_code):
+        self.driver.find_element(By.CSS_SELECTOR, '[data-test="postalCode"]').send_keys(
+            postal_code
+        )
+
+    def click_continue_button(self):
+        self.driver.find_element(By.CSS_SELECTOR, '[data-test="continue"]').click()
+
+    def click_finish_button(self):
+        self.driver.find_element(By.CSS_SELECTOR, '[data-test="finish"]').click()
+
+    def get_checkout_container(self):
+        return self.driver.find_element(
+            By.CSS_SELECTOR, '[data-test="checkout-complete-container"]'
+        ).is_displayed()
+
+    def complete_message(self):
+        return self.driver.find_element(
+            By.CSS_SELECTOR, '[data-test="complete-header"]'
+        ).text
